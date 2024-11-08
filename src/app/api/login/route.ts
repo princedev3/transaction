97add935991948623f2fe764a5d5779eddb0e22a
princedev3 <marvinprince232@gmail.com>
2024-11-08 08:32:25 +0200

    login/route

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 
-export const POST = async (req: Request, _res: NextResponse) => {
+export const POST = async (req: Request) => {
   try {
     const { email, password } = await req.json();
     const age = 60 * 60 * 24 * 7;
